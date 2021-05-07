a76f9febb9da8d2eca535aba71073890ef1869ab
alcurvelo <alberto.curvelo@widergy.com>
2021-05-07 11:01:51 -0300

    Modificando Ejercicio 2 - No usando funciones nativas

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,31 +1,46 @@
 //Funciones
 const f1 = (arrayE) => {
     if(arrayE.length>0){
-        arrayE.map((number,i)=>arrayE[i]=Math.floor(number/2))
+        for(let i=0;i<arrayE.length;i++){
+            arrayE[i]=Math.floor(arrayE[i]/2)
+        }
         return arrayE
     }else{
         return []
     }
 }
 const f2 = (arrayE, func) => {
-    arrayE.map((number,i)=>arrayE[i]=func(number))
+    for(let i=0;i<arrayE.length;i++){
+        arrayE[i]=func(arrayE[i])
+    }
     return arrayE
 }
 const f3 = (arrayE, n) => {
     var arrayNew=[]
-    arrayE.map(number=>{
-        if(number>n){
-            arrayNew.push(number)
+    for(let i=0;i<arrayE.length;i++){
+        if(arrayE[i]>n){
+            arrayNew.push(arrayE[i])
         }
-    })
+    }
     return arrayNew
 }
 const f4 = (arrayE, func) => {
-    return arrayE.filter(num=>func(num));
+    newArray=[]
+    for(let i=0;i<arrayE.length;i++){
+        if(func(arrayE[i])){
+            newArray.push(arrayE[i])
+        }
+    }
+    return newArray
 }
 const f5 = (arrayE,n) =>{
     //Buscando los numeros menores y agregandolos a un array, si existen
-    var newArrayMenores=arrayE.filter(num=>{if(num<n){return num}})
+    var newArrayMenores=[]
+    for(let i=0;i<arrayE.length;i++){
+        if(arrayE[i]<n){
+            newArrayMenores.push(arrayE[i])
+        }
+    }
     if(newArrayMenores.length>0){
         //retornando la primera incidencia
         return newArrayMenores[0]
@@ -36,14 +51,13 @@ const f5 = (arrayE,n) =>{
 }
 const f6 = (arrayE, func) => {
     //Llenando un array de los resultados devultos por la funcion del parametro
-    var newArrayResult=arrayE.filter(number=>func(number))
-    if(newArrayResult.length>0){
-        //Retornando la primera incidencia
-        return newArrayResult[0]
-    }else{
-        //Si no hay incidencias retorna -1
-        return -1
+    for(let i=0;i<arrayE.length;i++){
+        //Retorna el primero que cumpla
+        if(func(arrayE[i])){
+            return arrayE[i]
+        }
     }
-  }
-
-  test_ejercicio2(f1, f2, f3, f4, f5, f6);
+    //Si nungúno cumple retorna -1
+    return -1
+}
+test_ejercicio2(f1, f2, f3, f4, f5, f6);
