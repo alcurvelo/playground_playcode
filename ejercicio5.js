29459045cf7730fe14379e17ecf269c648f6971d
alcurvelo <alberto.curvelo@widergy.com>
2021-05-10 18:21:02 -0300

    Ejercicio 5 - Culminado

--- ejercicio5.js
+++ ejercicio5.js
@@ -4,12 +4,10 @@ const extraePalabras=(cadena)=>{
     let inicio=0
     for(let i=0;i<cadena.length;i++){
         if(cadena[i]===' '){
-            fin=i
-            arrayPalabras.push(cadena.substring(inicio,fin))
-            inicio=fin+1
+            arrayPalabras.push(cadena.substring(inicio,i))
+            inicio=i+1
         }else if(i===cadena.length-1){
-            fin=cadena.length
-            arrayPalabras.push(cadena.substring(inicio,fin))
+            arrayPalabras.push(cadena.substring(inicio,cadena.length))
         }
     }
     return arrayPalabras
@@ -25,7 +23,7 @@ const capitalizar = (cadena) => {
             //Edito la cadena, cambiando la primera por mayuscula, y el resto lo concateno igual
             arrayPalabras[i]=arrayPalabras[i][0].charAt().toUpperCase()+arrayPalabras[i].substring(1,arrayPalabras[i].length)
         }
-        newCadenaCapitalizada=newCadenaCapitalizada+space+arrayPalabras[i]
+        newCadenaCapitalizada+=(space+arrayPalabras[i])
     }
     return newCadenaCapitalizada
   }
@@ -36,24 +34,21 @@ const inverso = (cadena) => {
     for(let i=arrayPalabras.length-1;i>=0;i--){
         let space=' '
         if(newCadenaInversa.length===0) space=''
-        newCadenaInversa=newCadenaInversa+space+arrayPalabras[i]
+        newCadenaInversa+=(space+arrayPalabras[i])
     }
     return newCadenaInversa;
 }
 const compruebaCadenasIguales=(cadenaBase,cadenaComprueba)=>{
+    console.log(cadenaBase,cadenaComprueba)
     if(cadenaBase.length===cadenaComprueba.length){
         for(let i=0;i<cadenaComprueba.length;i++){
-            if(cadenaBase[i].charAt()===cadenaComprueba[i].charAt()){
-                if(i===cadenaComprueba.length-1){
-                    return true
-                }
-            }else{
+            if(cadenaBase[i].charAt()!==cadenaComprueba[i].charAt()){
                 return false
             }
         }
-    }else{
-        return false
+        return true
     }
+    return false
 }
 const generaCaracter=(caracter,numero)=>{
     let cadena=""
@@ -69,20 +64,22 @@ const sanitizar = (cadena, cadenaACambiar) => {
     for(let i=0;i<arrayPalabras.length;i++){
         let space=' '
         if(newCadena.length===0) space=''
+        //Si la segunda palabra es igual, la rellena con el caracter.
+        //Sí no, deja la palabra en la oración
         if(compruebaCadenasIguales(arrayPalabras[i],cadenaACambiar)){
-            newCadena=newCadena+space+generaCaracter('#',cadenaACambiar.length)
+            newCadena+=(space+generaCaracter('#',cadenaACambiar.length))
         }else{
-            newCadena=newCadena+space+arrayPalabras[i]
+            newCadena+=(space+arrayPalabras[i])
         }
     }
     return newCadena
 }
   
 const contarArrays = (arrayBase, arrayComprueba) => {
-    //Iniciando el array contador
+    //Iniciando el array contador con el tamaño del array comprobar
     let arrayCont=new Array(arrayComprueba.length)
     for(let i=0;i<arrayCont.length;i++)arrayCont[i]=0
-    //Recorirendo el array y comprobando palabra por palabra (0x1,0x2,0x3;1x1,1x2...n x n)
+    //Recorriendo el array y comprobando palabra por palabra (0x1,0x2,0x3;1x1,1x2...n x n)
     for(let i=0;i<arrayBase.length;i++){
         for(let j=0;j<arrayComprueba.length;j++){
             if(compruebaCadenasIguales(arrayBase[i],arrayComprueba[j])){
@@ -94,18 +91,19 @@ const contarArrays = (arrayBase, arrayComprueba) => {
 }
   
 const esSubSecuencia = (s1, s2) => {
-    if(s1.length>0){
-        console.log(s1,s2)
-        console.log(s1.substring(s1.length-s2.length,s1.length),s2)
-        //Según el tamaño de la cadena a comprobar, cortor el final de la cadena 1 y las compruebo
-        if(compruebaCadenasIguales(s1.substring(s1.length-s2.length,s1.length),s2)){
-            return true
-        }else{
-            return false
+    if(!s1)return false
+    //comprueba letra por letra hasta que encuentra 1 igual
+    for(let i=0;i<s1.length-1;i++){
+        for(let j=0;j<s2.length;j++){
+            //Si es igual busco comprobar ambas cadenas, si es igual retorna true
+            if(s1[i]===s2[j]&&compruebaCadenasIguales(s1.substring(i,(i+s2.length)),s2)){
+                return true
+            }
+            //Si la primera comprobacion no es igual sigue a la siguiente letra
+            break
         }
-    }else{
-        return false
     }
+    return false
 }
   
 test_ejercicio5(capitalizar, inverso, sanitizar, contarArrays, esSubSecuencia)
