43a711f4424c36757a1f3420165a0df4212ee8da
alcurvelo <alberto.curvelo@widergy.com>
2021-05-10 15:49:24 -0300

    Correcciones agregadas - ejercicio2

--- ejercicio2.js
+++ ejercicio2.js
@@ -7,51 +7,49 @@ const f1 = (arrayE) => {
     return arrayE
 }
 const f2 = (arrayE, func) => {
+    if(!arrayE||!func)return arrayE
     for(let i=0;i<arrayE.length;i++){
         arrayE[i]=func(arrayE[i])
     }
     return arrayE
 }
 const f3 = (arrayE, n) => {
-    var arrayNew=[]
-    for(let i=0;i<arrayE.length;i++){
-        if(arrayE[i]>n){
-            arrayNew.push(arrayE[i])
+    let arraySoloMAyoresQueN=[]
+    if(arrayE){
+        for(let i=0;i<arrayE.length;i++){
+            if(arrayE[i]>n){
+                arraySoloMAyoresQueN.push(arrayE[i])
+            }
         }
     }
-    return arrayNew
+    return arraySoloMAyoresQueN
 }
 const f4 = (arrayE, func) => {
+    if(arrayE||func){
     newArray=[]
-    for(let i=0;i<arrayE.length;i++){
-        if(func(arrayE[i])){
-            newArray.push(arrayE[i])
+        for(let i=0;i<arrayE.length;i++){
+            if(func(arrayE[i])){
+                newArray.push(arrayE[i])
+            }
         }
     }
     return newArray
 }
 const f5 = (arrayE,n) =>{
-    //Buscando los numeros menores y agregandolos a un array, si existen
-    var newArrayMenores=[]
-    for(let i=0;i<arrayE.length;i++){
-        if(arrayE[i]<n){
-            newArrayMenores.push(arrayE[i])
+    if(arrayE){
+        for(let i=0;i<arrayE.length;i++){
+            if(arrayE[i]<n){
+                return arrayE[i]
+            }
         }
     }
-    if(newArrayMenores.length>0){
-        //retornando la primera incidencia
-        return newArrayMenores[0]
-    }else{
-        //Si no hay elementos en el array retorna -1
-        return -1
-    }
+    return -1
 }
 const f6 = (arrayE, func) => {
-    //Llenando un array de los resultados devultos por la funcion del parametro
-    for(let i=0;i<arrayE.length;i++){
-        //Retorna el primero que cumpla
-        if(func(arrayE[i])){
-            return arrayE[i]
+    if(arrayE&&func){
+        for(let i=0;i<arrayE.length;i++){
+            //Retorna el primero que cumpla
+            if(func(arrayE[i])) return arrayE[i]
         }
     }
     //Si nungúno cumple retorna -1
