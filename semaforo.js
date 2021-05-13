8826d70e3c52546011c53f58f6116611ebb38925
alcurvelo <alberto.curvelo@widergy.com>
2021-05-13 17:37:29 -0300

    Modificando semaforo, versión 2

--- semaforo.js
+++ semaforo.js
@@ -4,50 +4,51 @@ const lanzarPromesa=(numeroDePromesa)=>{
         //A fines vizuales coloque el timeout para el color amarillo
         setTimeout(()=>{
         console.log(`Promesa del semaforo ${numeroDePromesa} en proceso`)
-        cambiarLuz(`luzAmarilla${numeroDePromesa}`)
+        cambiarLuz('luzAmarilla')
             setTimeout(()=>{
                 resolve("Promesa numero: " + numeroDePromesa + " resuelta");
-            },2000)
-        }, 1000);
+            },1000)
+        }, 2000);
     })
 }
-const generaSemaforo=(numero)=>{
-    let newSemaforo=document.createElement('div')
-    //Para dar tiempo a que se cree el nuevo semaforo
-    //Genere una promesa para que lo espere, y luego pueda cambiar las luces.
-    return new Promise((resolve, reject) => {
-        setTimeout(function() {
-            newSemaforo.innerHTML=`
-            <div id="sem${numero}" class="semaforo">
-                <div id="luzVerde${numero}" class="foco verde gris"></div>
-                <div id="luzAmarilla${numero}" class="foco amarillo gris"></div>
-                <div id="luzRoja${numero}" class="foco rojo gris"></div>
-            </div>`
-            boxSemaforos.appendChild(newSemaforo)
-            cambiarLuz(`luzVerde${numero}`)
-            resolve("Se creo el semaforo numero: " + numero);
-        }, 1000);
-    })
-}
-const cambiarLuz=(focoLuzId)=>{
-    let elementFoco=document.getElementById(`${focoLuzId}`)
+const cambiarLuz=(focoLuz)=>{
+    let elementFoco=document.getElementById(`${focoLuz}`)
     elementFoco.classList.remove('gris')
+}
+const reiniciaSemaforo=()=>{
+    let luces=["luzVerde","luzAmarilla","luzRoja"]
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            luces.map(luz=>document.getElementById(luz).classList.add('gris'))
+            resolve("Se inicia el semaforo numero: ")
+        },1000)
+    })
 }
 const semaforo=async(cantDePromesas)=>{
     for(let i=0;i<cantDePromesas;i++){
-        await generaSemaforo(i)
-        .then(semaforoCreado=>{
-            console.log(semaforoCreado)
-            cambiarLuz(`luzVerde${i}`)
-        })
-        await lanzarPromesa(i)
-        .then(resolve=>{
-            cambiarLuz(`luzRoja${i}`)
-            console.log(resolve+'\n/////////////////////////////////////////')
+        //Una promesa para reiniciar el semaforo
+        await reiniciaSemaforo()
+        .then(async reinicio=>{
+            document.getElementById('numeroPromesa').innerHTML=i+1
+            console.log(reinicio+i)
+            cambiarLuz('luzVerde')
+            await lanzarPromesa(i)
+            .then(resolve=>{
+                cambiarLuz('luzRoja')
+                console.log(resolve+'\n/////////////////////////////////////////')
+            })
         })
     }
 }
-semaforo(10)
+document.querySelector('.bCantidadPromesas').addEventListener('click', function(e){
+    e.preventDefault();
+    let bCantidadPromesas=document.getElementById('cantidadPromesas').value
+    if(bCantidadPromesas.length>0&&parseInt(bCantidadPromesas)>0){
+        semaforo(parseInt(bCantidadPromesas))
+    }else{
+        alert("Solo se admiten números mayores a 0.")
+    }
+})
   /*lanzarProcesos(7).forEach((promesa) => {
     promesa.then((mensaje) => {
       console.log(mensaje);
