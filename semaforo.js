3d8e02c3523f5ccf602dc79da16828d04c91e827
alcurvelo <alberto.curvelo@widergy.com>
2021-05-17 17:14:06 -0300

    Semaforo - V4

--- semaforo.js
+++ semaforo.js
@@ -1,70 +1,66 @@
-let ocupado=false
-let procesosEnEspera=[]
-let boxSemaforos=document.querySelector('.boxSemaforos')
-const lanzarPromesa=(numeroDePromesa)=>{
-    return new Promise((resolve, reject) => {
-        //A fines visuales coloque el timeout para el color amarillo
-        setTimeout(()=>{
-            let time=Math.random()*100
-            console.log(`Promesa del semaforo ${numeroDePromesa} en proceso`)
-            cambiarLuz('luzAmarilla')
-                setTimeout(()=>{
-                    resolve("Promesa numero: " + numeroDePromesa + " resuelta con el tiempo random: "+time);
-                },time)
-        }, 0);
-        //El tiempo deberia estar en 1000 o más para que se vean el cambio de colores.
-    })
-}
-const cambiarLuz=(focoLuz)=>{
-    let elementFoco=document.getElementById(`${focoLuz}`)
-    elementFoco.classList.remove('gris')
-}
-const reiniciaSemaforo=()=>{
-    let luces=["luzVerde","luzAmarilla","luzRoja"]
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            luces.map(luz=>document.getElementById(luz).classList.add('gris'))
-            resolve("Se inicia el semaforo numero: ")
-        }, 0)
-        //El tiempo deberia estar en 1000 o más para que se vean el cambio de colores.
-    })
-}
-const noOcupado=(element)=>{
-    if(!ocupado){
-        ocupado=true
-        procesoPorElemento(element)
-    }
-    else procesosAEsperar(element)
-}
-const procesosAEsperar=(element)=>{
-    if(element==='signal'&&procesosEnEspera.length){
-        noOcupado(procesosEnEspera.shift())
-    }else if(element!=='signal'){
-        procesosEnEspera.push(element)
-    }
-}
-const procesoPorElemento=(element)=>{
-    reiniciaSemaforo()
-    .then(reinicio=>{
-        document.getElementById('numeroPromesa').innerHTML=element+1
-        console.log(reinicio+element)
-        cambiarLuz('luzVerde')
-        lanzarPromesa(element)
-        .then(resolve=>{
-            cambiarLuz('luzRoja')
-            console.log(resolve+'\n/////////////////////////////////////////')
-            ocupado=false
-            procesosAEsperar('signal')
-        })
-    })
-}
 const semaforo=(cantDePromesas)=>{
-    //Guandando en un array
-    let cola=[]
-    for(let i=0;i<cantDePromesas;i++){
-        cola.push(i)
-        console.log(cola)
+    const lanzarPromesa=(numeroDePromesa)=>{
+        return new Promise((resolve, reject) => {
+            //A fines visuales coloque el timeout para el color amarillo
+            setTimeout(()=>{
+                let time=Math.random()*100
+                console.log(`Promesa del semaforo ${numeroDePromesa} en proceso`)
+                cambiarLuz('luzAmarilla')
+                    setTimeout(()=>{
+                        resolve("Promesa numero: " + numeroDePromesa + " resuelta con el tiempo random: "+time);
+                    },time)
+            }, 0);
+            //El tiempo deberia estar en 1000 o más para que se vean el cambio de colores.
+        })
     }
+    const cambiarLuz=(focoLuz)=>{
+        let elementFoco=document.getElementById(`${focoLuz}`)
+        elementFoco.classList.remove('gris')
+    }
+    const reiniciaSemaforo=()=>{
+        let luces=["luzVerde","luzAmarilla","luzRoja"]
+        return new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                luces.map(luz=>document.getElementById(luz).classList.add('gris'))
+                resolve("Se inicia el semaforo numero: ")
+            }, 0)
+            //El tiempo deberia estar en 1000 o más para que se vean el cambio de colores.
+        })
+    }
+    const noOcupado=(element)=>{
+        if(!ocupado){
+            ocupado=true
+            procesoPorElemento(element)
+        }
+        else procesosAEsperar(element)
+    }
+    const procesosAEsperar=(element)=>{
+        if(element==='signal'&&procesosEnEspera.length){
+            noOcupado(procesosEnEspera.shift())
+        }else if(element!=='signal'){
+            procesosEnEspera.push(element)
+        }
+    }
+    const procesoPorElemento=(element)=>{
+        reiniciaSemaforo()
+        .then(reinicio=>{
+            document.getElementById('numeroPromesa').innerHTML=element+1
+            console.log(reinicio+element)
+            cambiarLuz('luzVerde')
+            lanzarPromesa(element)
+            .then(resolve=>{
+                cambiarLuz('luzRoja')
+                console.log(resolve+'\n/////////////////////////////////////////')
+                ocupado=false
+                procesosAEsperar('signal')
+            })
+        })
+    }
+    //Guandando en un array y ejecutando
+    let ocupado=false
+    let procesosEnEspera=[]
+    let cola=[]
+    for(let i=0;i<cantDePromesas;i++)cola.push(i)
     cola.forEach(element=>{
         noOcupado(element)
     })
